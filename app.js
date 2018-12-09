db87cd0b4575174d19874e6d5bbfe42a4504084f
SemyonMerkulov <semen.merkulov@gmail.com>
2018-12-09 18:29:53 +0300

    add fullscreen photo component

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var appId = 'd6d78c3b896ec7c3f7538ea857d363f3474b7f13caba938c19b1c611d175fdd6';
 
+Vue.component('fullscreen', {
+	props: ['photo'],
+	template: '#fullscreen',
+	methods: {
+		close: function() {
+			this.$emit('close');
+		}
+	}
+})
+
 new Vue({
 	el: '#app',
 	data: {
+		activeFullscreen: false,
+		fullPhotoLink: '',
 		photos: [],
 		totalPhotos: 0,
 		perPage: 9,
@@ -25,6 +37,15 @@ new Vue({
 				.catch(error => {
 				    console.log(error)
 				 })
+		},
+		openFullscreen: function(url) {
+			this.activeFullscreen = true;
+			this.fullPhotoLink = url;
+			document.body.style.overflow = 'hidden';
+		},
+		closeFullscreen: function() {
+			this.activeFullscreen = false;
+			document.body.style.overflow = 'auto';
 		}
 	},
 	created: function() {
